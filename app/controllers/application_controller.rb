5fab0b19247ec26b44b57e5384315f2c910fbeac
Travis Berry <travisberry@travisberry.com>
2011-08-20 19:31:42 -0600

    bug fix for redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :redirect_to_https
+  #before_filter :redirect_to_https
   before_filter :authenticate_user!
 
   # Handle unauthorized access with a good old fashioned 'forbidden'
@@ -23,8 +23,8 @@ class ApplicationController < ActionController::Base
       end
     end
   end
-  def redirect_to_https
-      redirect_to :protocol => "https://" 
-      #redirect_to url_for params.merge({:protocol => 'https://'})
-  end
+  # def redirect_to_https
+  #     redirect_to :protocol => "https://" 
+  #     #redirect_to url_for params.merge({:protocol => 'https://'})
+  # end
 end
