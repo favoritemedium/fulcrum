e82875aeff7dbacbf46d7829d5c0535a7721b952
Travis Berry <travisberry@travisberry.com>
2011-08-20 19:20:29 -0600

    think i had config wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ::SslRequirement
 
-  ssl_allowed :all
+  ssl_required :all
   protect_from_forgery
 
   before_filter :authenticate_user!
